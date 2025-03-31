549f070d08722eae03869c9c381bd26cc32c2833
henrique <henriquenakashima05@gmail.com>
2025-03-31 09:07:51 -0300

    criando variaveis

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+// DECLARACAO DE VARIAVEL
+
+var nome = "fiap"
+console.log(nome)
+
+let idade = 19
+console.log(idade)
+
+const sobrenome = "Fiapinho"
+console.log(sobrenome)
+
+//declarando um variavel indefinido
+let nome1;
+console.log(nome1)
+
+//declarando uma variavel nula
+let nome2 = null;
+console.log(nome2)
